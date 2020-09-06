78a75192cf56b65461e53fa522af3b32f7bcce3f
leandrorochaadm <leandrorochaadm@gmail.com>
2020-09-06 02:09:47 -0400

    add metodo delete ao EventoController

--- src/main/java/com/temaula/rdb/controller/EventoController.java
+++ src/main/java/com/temaula/rdb/controller/EventoController.java
@@ -2,6 +2,7 @@ package com.temaula.rdb.controller;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -15,33 +16,41 @@ import com.temaula.rdb.service.EventoService;
 
 @Path("/eventos")
 public class EventoController {
-	
+
 	@Inject
 	private EventoService eventoService;
-	
+
 	@GET
-	@Path( "/lista" )
-	@Produces( value = MediaType.APPLICATION_JSON )
-	public Response listarTodos(){
-		return Response.ok( eventoService.listarTodos() ).build();
+	@Path("/lista")
+	@Produces(value = MediaType.APPLICATION_JSON)
+	public Response listarTodos() {
+		return Response.ok(eventoService.listarTodos()).build();
 	}
-	
+
 	@GET
-	@Path( "/lista/{id}" )
-	@Produces( value = MediaType.APPLICATION_JSON )
-	public Response pesquisarId( @PathParam( "id" ) int id ) {
-		
-		if( eventoService.pesquisarId( id ) == null  ){
+	@Path("/{id}")
+	@Produces(value = MediaType.APPLICATION_JSON)
+	public Response pesquisarId(@PathParam("id") int id) {
+
+		if (eventoService.pesquisarId(id) == null) {
 			return Response.status(Response.Status.NOT_FOUND).entity("{ \"erro\" : \"ID não encontrado\" }").build();
 		}
-		return Response.ok( eventoService.pesquisarId( id ) ).build();
+		return Response.ok(eventoService.pesquisarId(id)).build();
+	}
+
+	@POST
+	@Path("/inserir")
+	@Consumes(value = MediaType.APPLICATION_JSON)
+	public Response inserir(Evento evento) {
+		eventoService.inserir(evento);
+		return Response.status(201).build();
 	}
 	
-	@POST
-	@Path( "/inserir" )
-	@Consumes( value = MediaType.APPLICATION_JSON )
-	public Response inserir( Evento evento) {
-		eventoService.inserir( evento );
-		return Response.status(201).build();
+	@DELETE
+	@Path("/{id}")
+	@Consumes(value = MediaType.APPLICATION_JSON)
+	public Response deletar(@PathParam("id") int id) {
+		eventoService.deletar(id);
+		return Response.status(202).build();
 	}
 }
--- src/main/java/com/temaula/rdb/service/EventoService.java
+++ src/main/java/com/temaula/rdb/service/EventoService.java
@@ -11,114 +11,77 @@ import com.temaula.rdb.model.PessoaModel;
 
 @Stateless
 public class EventoService {
-	List< Evento > lista = new ArrayList< Evento >();
-	
-	public List< Evento > listarTodos(){
-		
-		//lista = new ArrayList< Evento >();
-		
+	List<Evento> lista = new ArrayList<Evento>();
+
+	// Inicializando dados
+	public EventoService() {
 		Evento e1 = new Evento();
 		e1.setId(1);
-		e1.setDataInicio( LocalDate.now() );
-		e1.setDataFim( LocalDate.now() );
-		e1.setDataCriacao( LocalDate.of( 2020, 01, 15 ) );
-		e1.setAtivo( true );
-		e1.setDescricao( "Sopao do Bras" );
-		e1.setItensDoacao( null );
-		
+		e1.setDataInicio(LocalDate.now());
+		e1.setDataFim(LocalDate.now());
+		e1.setDataCriacao(LocalDate.of(2020, 01, 15));
+		e1.setAtivo(true);
+		e1.setDescricao("Sopao do Bras");
+		e1.setItensDoacao(null);
+
 		PessoaModel pessoa = new PessoaModel();
-		pessoa.setId( 1L );
-		pessoa.setNome( "Evandro" );
-		pessoa.setEmail( "email@email.com" );
-		pessoa.setSenha( "1234" );
-		pessoa.setTelefone( "(11) 91234-5678" );
-		pessoa.setEndereco( "Rua da Paroquia, n1" );
-		e1.setAutor( pessoa );
-		e1.setUrlImagem( "/imagem/sopao1.png" );
-		
-		lista.add( e1 );
-		
-		Evento e2 = new Evento();
-		e2.setId(2);
-		e2.setDataInicio( LocalDate.of( 2020, 02, 15) );
-		e2.setDataFim( LocalDate.of( 2020, 02, 18 ) );
-		e2.setDataCriacao( LocalDate.of( 2020, 01, 15 ) );
-		e2.setAtivo( true );
-		e2.setDescricao( "Sopao do Se" );
-		e2.setItensDoacao( null );
-		
-		PessoaModel pessoa2 = new PessoaModel();
-		pessoa2.setId( 1L );
-		pessoa2.setNome( "Fredegundo" );
-		pessoa2.setEmail( "email@email.com" );
-		pessoa2.setSenha( "1234" );
-		pessoa2.setTelefone( "(11) 91234-5678" );
-		pessoa2.setEndereco( "Rua da Paroquia, n7" );
-		e2.setAutor( pessoa2 );
-		e2.setUrlImagem( "/imagem/sopao2.png" );
-		
-		lista.add( e2 );
-		
-		return lista;
-	}
-	
-public Evento pesquisarId( int id ){
-		
-		//lista = new ArrayList< Evento >();
-		
-		Evento e1 = new Evento();
-		e1.setId(1);
-		e1.setDataInicio( LocalDate.now() );
-		e1.setDataFim( LocalDate.now() );
-		e1.setDataCriacao( LocalDate.of( 2020, 01, 15 ) );
-		e1.setAtivo( true );
-		e1.setDescricao( "Sopao do Bras" );
-		e1.setItensDoacao( null );
-		
-		PessoaModel pessoa = new PessoaModel();
-		pessoa.setId( 1L );
-		pessoa.setNome( "Evandro" );
-		pessoa.setEmail( "email@email.com" );
-		pessoa.setSenha( "1234" );
-		pessoa.setTelefone( "(11) 91234-5678" );
-		pessoa.setEndereco( "Rua da Paroquia, n1" );
-		e1.setAutor( pessoa );
-		e1.setUrlImagem( "/imagem/sopao1.png" );
-		
-		lista.add( e1 );
+		pessoa.setId(1L);
+		pessoa.setNome("Evandro");
+		pessoa.setEmail("email@email.com");
+		pessoa.setSenha("1234");
+		pessoa.setTelefone("(11) 91234-5678");
+		pessoa.setEndereco("Rua da Paroquia, n1");
+		e1.setAutor(pessoa);
+		e1.setUrlImagem("/imagem/sopao1.png");
+
+		lista.add(e1);
 
 		Evento e2 = new Evento();
 		e2.setId(2);
-		e2.setDataInicio( LocalDate.of( 2020, 02, 15) );
-		e2.setDataFim( LocalDate.of( 2020, 02, 18 ) );
-		e2.setDataCriacao( LocalDate.of( 2020, 01, 15 ) );
-		e2.setAtivo( true );
-		e2.setDescricao( "Sopao do Se" );
-		e2.setItensDoacao( null );
-		
+		e2.setDataInicio(LocalDate.of(2020, 02, 15));
+		e2.setDataFim(LocalDate.of(2020, 02, 18));
+		e2.setDataCriacao(LocalDate.of(2020, 01, 15));
+		e2.setAtivo(true);
+		e2.setDescricao("Sopao do Se");
+		e2.setItensDoacao(null);
+
 		PessoaModel pessoa2 = new PessoaModel();
-		pessoa2.setId( 1L );
-		pessoa2.setNome( "Fredegundo" );
-		pessoa2.setEmail( "email@email.com" );
-		pessoa2.setSenha( "1234" );
-		pessoa2.setTelefone( "(11) 91234-5678" );
-		pessoa2.setEndereco( "Rua da Paroquia, n7" );
-		e2.setAutor( pessoa2 );
-		e2.setUrlImagem( "/imagem/sopao2.png" );
-		
-		lista.add( e2 );
-		
+		pessoa2.setId(1L);
+		pessoa2.setNome("Fredegundo");
+		pessoa2.setEmail("email@email.com");
+		pessoa2.setSenha("1234");
+		pessoa2.setTelefone("(11) 91234-5678");
+		pessoa2.setEndereco("Rua da Paroquia, n7");
+		e2.setAutor(pessoa2);
+		e2.setUrlImagem("/imagem/sopao2.png");
+
+		lista.add(e2);
+	}
+
+	public List<Evento> listarTodos() {
+
+		return lista;
+	}
+
+	public Evento pesquisarId(int id) {
+
 		for (Evento evento : lista) {
-			if( evento.getId() == id ) {
+			if (evento.getId() == id) {
 				return evento;
 			}
 		}
-			
-			
+
 		return null;
 	}
 
-	public void inserir( Evento evento) {
+	public void inserir(Evento evento) {
 		lista.add(evento);
+	}
+	
+	public void deletar(int id) {
+		Evento e = pesquisarId(id); 
+		if( e != null) {
+			lista.remove(e);
+		}
 	}
 }
